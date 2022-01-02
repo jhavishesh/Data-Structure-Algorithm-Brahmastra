99d0f5cbe328800507308ec814c2d6c65f8b24a3
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-02 18:35:06 +0530

    Problem-38

---
+++ Array/Problem-38 Three way partitioning.cpp
@@ -0,0 +1,44 @@
+/* https://practice.geeksforgeeks.org/problems/three-way-partitioning/1# */
+
+
+//  Naive Solution:
+
+void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        // code here 
+       sort(array.begin(),array.end());
+        
+    }
+
+
+//Optimal Solution: O(N)  
+//Apply the DNF Algorithm 
+
+
+void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        // code here 
+        int low=0;
+        int mid=0;
+        int high=array.size()-1;
+        
+        while(mid<=high){
+            
+            if(array[mid]<a){
+                
+                swap(array[low],array[mid]);
+                mid++;
+                low++;
+            }
+            else if(array[mid]>b){
+                swap(array[mid],array[high]);
+                high--;
+            }
+            else
+            mid++;
+                
+        }
+    }
+
+
+
