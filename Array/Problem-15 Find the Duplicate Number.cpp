29671e33e075f90313dba0bd685ae3caacacbe7d
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-01-22 15:28:08 +0530

    Find the Duplicate Number

--- Array/Problem-15 Find the Duplicate Number.cpp
+++ Array/Problem-15 Find the Duplicate Number.cpp
@@ -19,5 +19,31 @@
         return res;
     }
         
-    
+  /* https://practice.geeksforgeeks.org/problems/find-duplicates-in-an-array/1#  */
+
+//Solution:
+
+ vector<int> duplicates(int arr[], int n) {
+        unordered_map <int,int>mp;
+        
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;   //track the count of element
+        }
+        
+        vector<int>res;
+        for(auto it:mp)
+        {
+             if(it.second>1) 
+                res.push_back(it.first);  // push element whose count>1 
+            
+        }
+        if(res.size()==0)    // if all the elements are distinct then return -1
+            res.push_back(-1);
+        
+        sort(res.begin(),res.end());   // sort the result as it demand of question
+        return res;
+        
+        
+    }
 
