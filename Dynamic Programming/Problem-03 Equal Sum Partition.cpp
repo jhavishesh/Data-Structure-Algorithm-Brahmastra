4d570257439308e9aa8ab855fe3fc70d199793c3
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-03 12:50:07 +0530

    Problem-03

--- Dynamic Programming/Problem-03 Equal Sum Partition.cpp
+++ Dynamic Programming/Problem-03 Equal Sum Partition.cpp
@@ -59,3 +59,45 @@ bool helper(int N,int arr[],int sum)
         return 0;
     }
 };
+
+//Another Simple Approach 
+
+class Solution{
+public:
+    int dp[101][10001];
+    bool helper(int N,int arr[],int sum)
+    {
+            
+        if(sum==0)
+            return true;
+        
+        if(N==0)
+            return false;
+        
+        if(dp[N][sum]!=-1)
+            return dp[N][sum];
+        
+        if(arr[N-1]<=sum)
+            return dp[N][sum]=helper(N-1,arr,sum-arr[N-1]) || helper(N-1,arr,sum);
+        
+        else if(arr[N-1]>sum)
+            return dp[N][sum]=helper(N-1,arr,sum);
+        
+        
+    }
+    bool isSubsetSum(int N,int arr[],int sum)
+    {
+        memset(dp,-1,sizeof(dp));
+        return helper(N,arr,sum);
+    }
+    int equalPartition(int N, int arr[])
+    {
+       long long int sum(0);
+        sum=accumulate(arr,arr+N,sum);
+        
+        if(sum%2!=0)
+            return false;
+        
+        else if(sum%2==0)
+            return isSubsetSum(N,arr,sum/2);
+    }
