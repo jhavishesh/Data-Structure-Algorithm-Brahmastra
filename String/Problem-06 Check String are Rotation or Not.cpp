4ebc53709b79f4f48b643ff590b33b7785a1ba29
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-03-22 19:50:04 +0530

    Problem-06.cpp

---
+++ String/Problem-06 Check String are Rotation or Not.cpp
@@ -0,0 +1,29 @@
+/* https://www.geeksforgeeks.org/a-program-to-check-if-strings-are-rotations-of-each-other/ */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool arerotation(string str1,string str2){
+
+  int n1=str1.length();//find the size of str1
+  int n2=str2.length();//find the size of str2
+
+  string temp =str1+str1;
+  if(n1!=n2) //if size is not equal then they can't form rotation
+    return false;
+  
+  return(temp.find(str2)!=string::npos);//find substr of str2 in temp
+}
+int main()
+{
+  string str1,str2;
+  getline(cin,str1);
+  getline(cin,str2);
+  if(arerotation(str1,str2))
+      cout<<"Rotation of one another";
+  else
+      cout<<"Not a Rotation of one another";
+
+return 0;
+}
