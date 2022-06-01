fa5a3ad04fad6d8001ae71e2f2ce1f52fae49c7b
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-06-01 19:28:29 +0530

    Problem-08

---
+++ Tree/Problem-08 Lowest Common Ancestor.cpp
@@ -0,0 +1,30 @@
+
+
+//Solution:
+
+class Solution
+{
+    public:
+    //Function to return the lowest common ancestor in a Binary Tree.
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       if(!root)    // if(root==NULL)
+            return NULL ;
+        
+        
+        if(root->data==n1 || root->data==n2)
+            return root;
+        
+        Node *lca1=lca(root->left,n1,n2);
+        Node *lca2=lca(root->right,n1,n2);
+        
+        if(lca1 and lca2 )  //If(lca!=NULL and lc2!=NULL)
+            return root;
+        
+        if(lca1)
+            return lca1;
+        
+        else
+            return lca2;
+    }
+};
