4fe66211fb8705b6a822c0267ee48463f189a3f5
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-06-19 23:53:41 +0530

    P1.cpp

---
+++ Leetcode Weekly Contest/Weekly Contest 298/Greatest English Letter in Upper and Lower Case.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case/
+
+
+//Solution:
+
+/* We have to find the greatest character that is being repeated.
+So we start checking from z to a if there exist an uppercase for that lowercase alphabet.
+If there exists, it will be the greatest one as we have started from 'z' that is biggest.  */
+
+
+string greatestLetter(string s) {
+        string a="";
+        for(char c='z';c>='a';c--)
+        {
+            if(find(s.begin(), s.end(), c)!=s.end() && find(s.begin(), s.end(), toupper(c))!=s.end())
+            {
+                a=toupper(c);
+                return a;
+            }
+        }
+        return "";
+    }
