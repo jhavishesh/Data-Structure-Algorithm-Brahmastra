ee8d1195bdb02b9f74c742731b8d1f1fedde8b73
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-06-26 22:22:04 +0530

    Problem-43

---
+++ Array/Problem-43 Merge Without Extra Space.cpp
@@ -0,0 +1,20 @@
+https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1#
+
+//Solution:
+
+ void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int i(n-1),j(0);
+            
+            while(i>=0 and j<m)
+            {
+                if(arr1[i]>arr2[j])
+                    swap(arr1[i--],arr2[j++]);
+                
+                else
+                    break;
+            }
+            
+            sort(arr1,arr1+n);
+            sort(arr2,arr2+m);
+        }   
