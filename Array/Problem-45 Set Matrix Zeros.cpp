36cdc6fbdcdead34f63bb80b2b0db5d2fc59dab4
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-06-29 20:36:04 +0530

    Problem-45

---
+++ Array/Problem-45 Set Matrix Zeros.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/set-matrix-zeroes/submissions/
+
+//Solution:
+
+ void setZeroes(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        vector<int> row(n,-1) ,col(m,-1);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=0;
+                    col[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(row[i]==0 || col[j]==0)
+                    matrix[i][j]=0;
+            }
+        }
+    }
