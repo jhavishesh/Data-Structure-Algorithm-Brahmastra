44f4f87b9cac43324f5f806a2d57723cdd5c2b8e
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-06-29 20:34:51 +0530

    Problem-44

---
+++ Array/Problem-44 Pascal Triangle.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/pascals-triangle/
+
+//Solution:
+
+vector<vector<int>> generate(int numRows) {
+     
+        vector<vector<int>>res(numRows);
+        
+        for(int i=0;i<numRows;i++)
+        {
+            res[i].resize(i+1);
+            res[i][0]=res[i][i]=1;
+            
+            
+            for(int j=1;j<i;j++)
+            {
+                res[i][j]=res[i-1][j-1]+res[i-1][j];
+            }
+        }
+        return res;
+    }
