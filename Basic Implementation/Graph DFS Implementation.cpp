c3c06d35833a2b61ff6f42a95d6247be994a81dd
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-07-05 18:15:56 +0530

    DFS.cpp

---
+++ Basic Implementation/Graph DFS Implementation.cpp
@@ -0,0 +1,17 @@
+
+ void dfs(int source,vector<int>&ans,vector<bool>&visited,vector<int>adj[])
+    {
+        ans.push_back(source);
+        visited[source]=true;
+        for(auto it:adj[source])
+        {
+            if(visited[it]==false)
+            dfs(it,ans,visited,adj);
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<bool>visited(V,false);
+        vector<int >ans;
+        dfs(0,ans,visited,adj);
+        return ans;
+    }
