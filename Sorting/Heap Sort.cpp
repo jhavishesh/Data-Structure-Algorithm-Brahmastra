47b2a2c639226fc5cd13c8c1f6fc1cee8e7f7553
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-07-17 21:16:45 +0530

    Heap Sort

---
+++ Sorting/Heap Sort.cpp
@@ -0,0 +1,43 @@
+ void heapify(int arr[], int n, int i)  
+    {
+        int left=2*i+1;
+        int right=2*i+2;
+        int lar=i;
+        
+        if(left<n and arr[left]>arr[lar])
+            lar=left;
+        
+        if(right<n and arr[right]>arr[lar])
+            lar=right;
+        
+        if(lar!=i)
+        {
+            swap(arr[lar],arr[i]);
+            heapify(arr,n,lar);
+        }
+        
+    }
+
+    public:
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)  
+    { 
+        for(int i=(n/2)-1;i>=0;i--)
+        {
+            heapify(arr,n,i);
+        }
+    }
+
+    
+    public:
+    //Function to sort an array using Heap Sort.
+    void heapSort(int arr[], int n)
+    {
+        buildHeap(arr,n);
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            swap(arr[i],arr[0]);
+            heapify(arr,i,0);
+        }
+    }
