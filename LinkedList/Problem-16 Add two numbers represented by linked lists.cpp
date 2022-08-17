cf89eaad52c512a9daf98cfc1a83032c1d6b38be
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-08-17 13:40:55 +0530

    Problem-16

---
+++ LinkedList/Problem-16 Add two numbers represented by linked lists.cpp
@@ -0,0 +1,60 @@
+/* https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1
+
+Solution:
+
+//Approach:
+Step 1: Reverse both the List
+Step 2: add both the list in sum till the NULL
+Step 3: add carry of addition of number
+Step 4 : reverse the result                           */
+
+    Node* reverse (Node* head){
+         
+    Node* curr= head;
+    Node* prev= nullptr;
+    Node* next= nullptr;
+         
+   while (curr != nullptr){
+             
+   next= curr->next;
+   curr->next= prev;
+             
+    prev= curr;
+    curr= next;
+    }
+         
+    return prev;
+     }
+
+    struct Node* addTwoLists(struct Node* l1, struct Node* l2)
+    {
+    
+     l1=reverse(l1);
+     l2=reverse(l2);
+     
+     Node *res=new Node(0);
+     Node *curr=res;
+     int sum(0);
+     
+     while(l1 or l2 or sum)
+     {
+         if(l1)
+         {
+             sum+=l1->data;
+             l1=l1->next;
+         }
+        
+        if(l2)
+        {
+            sum+=l2->data;
+            l2=l2->next;
+        }
+        curr->next=new Node(sum%10);
+        sum/=10;
+        curr=curr->next;
+        
+        
+     }
+     return reverse(res->next) ;
+     
+    }
