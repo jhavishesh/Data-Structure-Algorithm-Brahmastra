e596ba5a8d21f95539ef7f28a100afd1eb329c80
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2022-11-25 23:57:01 +0530

    Problem-16

--- LinkedList/Problem-16 Add two numbers represented by linked lists.cpp
+++ LinkedList/Problem-16 Add two numbers represented by linked lists.cpp
@@ -58,3 +58,37 @@ Step 4 : reverse the result                           */
      return reverse(res->next) ;
      
     }
+
+
+
+
+//Leetcode Version:
+
+ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode *l3=new ListNode(0),*curr=l3;
+        
+        int sum(0);
+        
+        while(l1 || l2 || sum)
+        {
+            if(l1)
+            {
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            
+            if(l2)
+            {
+                sum+=l2->val;
+                l2=l2->next;
+                
+            }
+            
+            curr->next =new ListNode(sum%10);
+            sum=sum/10;
+            curr=curr->next;
+            
+        }
+        return l3->next;
+    }
