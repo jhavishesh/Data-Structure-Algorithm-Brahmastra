84dc0a6a320b241e6650e076d0f5558ae4e1089c
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2023-02-21 17:20:37 +0530

    Create Problem-13 Generate Parentheses.cpp

---
+++ Recursion/Problem-13 Generate Parentheses.cpp
@@ -0,0 +1,25 @@
+ void findall(string s,int open,int close,vector<string>&res,int n)
+    {
+        
+        if(s.length()==2*n)
+        {
+            res.push_back(s);
+            
+            return;
+        }
+        
+        if(open<n)
+            findall(s+"(",open+1,close,res,n);
+        
+        if(close<open)
+            findall(s+")",open,close+1,res,n);
+    }
+    vector<string> AllParenthesis(int n) 
+    {
+        vector<string>res;
+        
+        findall("(",1,0,res,n);
+        
+        
+        return res;
+    }
