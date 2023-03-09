04737a0b731afc644b01dcda30e9922bde9e28d1
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2023-03-09 23:43:52 +0530

    Problem -51

---
+++ Array/Problem -51 Move Zeros to End.cpp
@@ -0,0 +1,13 @@
+	void pushZerosToEnd(int arr[], int n) {
+	    
+	    int i(0);
+	    
+	    for(int j=0;j<n;j++)
+	    {
+	        if(arr[j]!=0)
+	           {
+	               swap(arr[i],arr[j]);
+	               i++;
+	           }
+	    }
+	}
