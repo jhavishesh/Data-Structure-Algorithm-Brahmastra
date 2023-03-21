a04d8e295a154ff01483c9aa0b0f56bdb47bd198
Vishesh Jha <61939693+jhavishesh@users.noreply.github.com>
2023-03-21 11:22:19 +0530

    Problem-17

--- Array/Problem-17 Merge Without Extra Space.cpp
+++ Array/Problem-17 Merge Without Extra Space.cpp
@@ -28,3 +28,21 @@ void merge(int arr1[], int arr2[], int n, int m) {
                  k++;
                 }
 }
+
+ void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            
+            int i(n-1),j(0);
+            
+            while(i>=0 and j<m)
+            {
+                if(arr1[i]>arr2[j])
+                    swap(arr1[i--],arr2[j++]);
+                
+                else
+                    break;
+                    
+            }
+            sort(arr1,arr1+n);
+            sort(arr2,arr2+m);
+        } 
